96fa139016654b8b8a4e5842bc3af9600fb22476
SmartAlekz <alexmbastos@yahoo.com.br>
2024-04-13 11:20:28 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+function showPopup(bool) {
+  if (bool) {
+    document.getElementById('popup').style.visibility = 'visible'
+  } else {
+    document.getElementById('popup').style.visibility = 'hidden'
+  }
+}
